286f0d5589057ea9bd72a1ef564e524d3269c48c
erikonasz <arcikauskas.erikas@gmail.com>
2022-12-03 22:59:53 +0200

    Fully functional post autofollower (2 Option)

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def login():
     username = driver.find_element(By.NAME, 'username')
     password = driver.find_element(By.NAME, 'password')
 
+
     username.click()
     username.send_keys('username')
     password.click()
-    password.send_keys('pass')
+    password.send_keys('password')
     time.sleep(3)
 
     login_B = driver.find_element(By.XPATH, '//div[text()="Log in"]')
@@ -35,8 +36,8 @@ def login():
         saveinfo.click()
         time.sleep(6)
     except:
-        print('Button not found. Skipping save info button.')
-        pass
+        print('Button not found. Wrong Username/Password?')
+        quit()
     try:
         notifics = driver.find_element(By.XPATH, '//button[text()="Not Now"]')
         notifics.click()
@@ -45,19 +46,26 @@ def login():
     except:
         print('Button not found. Skipping notifications button.')
         pass
-    pplinput = int(input("How many people would you like to follow?"))
 
 def Randomsleep():
-    _sleep = random.randint(3,7)
+    _sleep = random.randint(10,20)
     time.sleep(_sleep)
 
 def SelectAutof():
-    selectf = input('Press 1 to Autofollow from a page. Press 2 to AutoFollow from the post(liked post people):  ')
+    print("Please select the function: ")
+    print("1. AutoFollow people who followed particular page")
+    print("2. AutoFollow everyone who liked the particular post")
+    selectf = input()
     if selectf == '1':
         igpagef = input("Please enter the IG page username: ")
         driver.get(f"https://www.instagram.com/{igpagef}/followers")
         time.sleep(8)
         Follow()
+    if selectf == '2':
+        postcode = input("Please enter the IG post code https://www.instagram.com/p/xxxxxxxx/: ")
+        driver.get(f"https://www.instagram.com/p/{postcode}/liked_by")
+        time.sleep(8)
+        FollowP()
 
 def Follow():
     scroll = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
@@ -70,20 +78,30 @@ def Follow():
             for button in fbuttons:
                 if button.text == 'Follow':
                     button.click()
+                    print("Followed!")
                     Randomsleep()
-                    time.sleep(3)
-            #if FC == pplinput:
-                #print("Reached the desired follow amount!")
-                #SelectAutof()
         except:
             print("Can't find follow button, all people already followed?")
             time.sleep(3)
-        try:
-            cancelfolw = driver.find_element(By.XPATH, '//button[text()="Cancel"]')
-            cancelfolw.click()
-        finally:
+            SelectAutof()
+
+def FollowP():
+    scroll1 = driver.find_element(By.XPATH,'/html')
+    while True:
+        for _ in range(2):
             time.sleep(5)
-            Follow()
+            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll1)
+        fbuttonsp = driver.find_elements(By.XPATH, '//div[text()="Follow"]')
+        try:
+            for button in fbuttonsp:
+                if button.text == 'Follow':
+                    button.click()
+                    print("Followed!")
+                    Randomsleep()
+        except:
+            print("Can't find follow button, all people already followed?")
+            time.sleep(3)
+            SelectAutof()
 
 login()
 SelectAutof()
